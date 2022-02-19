02d5383872666de07709621e6f036018f050feb0
vdumitru <vdumitru@axway.com>
2022-02-19 14:49:08 +0200

    adding mock server

---
+++ src/backend/server.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const mockData = require('./MOCK_DATA.json');
+
+const app = express();
+
+// TODO document
+// TODO implement other needed rest API endpoints
+
+app.get('/api/v1/users', (req, res) => {
+	return res.send(mockData);
+});
+
+// app.post('/', (req, res) => {
+// 	return res.send('Received a POST HTTP method');
+// });
+//
+// app.put('/', (req, res) => {
+// 	return res.send('Received a PUT HTTP method');
+// });
+//
+// app.delete('/', (req, res) => {
+// 	return res.send('Received a DELETE HTTP method');
+// });
+
+app.listen(9090, () =>
+	console.log(`Server listening on port 9090!`),
+);
